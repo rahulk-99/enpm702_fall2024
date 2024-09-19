ccf2531b2b4f5866c952de2ed5c09be8ff4750ac
Adon <adon.sf4@gmail.com>
2024-09-18 23:50:26 -0400

    Added files for lecture 5

---
+++ lecture5/src/lecture5.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file lecture5.cpp
+ * @author Zeid Kootbally (zeidk@umd.edu)
+ * @brief Code for lecture on Functions
+ * @version 0.1
+ * @date 2024-09-18
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+int main() {
+
+    //</> 1
+    //=====================
+    
+}
