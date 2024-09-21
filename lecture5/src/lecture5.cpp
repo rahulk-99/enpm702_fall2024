edeeb9c49f873d317f5f5f19447b4a459e70cc97
zeid <zeid.kootbally@nist.gov>
2024-09-21 18:30:24 -0400

    Working on lecture 5

--- lecture5/src/lecture5.cpp
+++ lecture5/src/lecture5.cpp
@@ -10,9 +10,44 @@
  */
 #include <iostream>
 
-int main() {
+//</> 1
+//=====================
 
-    //</> 1
-    //=====================
-    
+// int main() {
+// }
+
+//</> 2
+//=====================
+
+// // function declaration/prototype
+// int add(int a, int b);
+
+// // function definition/implementation
+// int add(int a, int b) {
+//     return a + b;
+// }
+
+// int main() {
+//     std::cout << add(1, 2) << '\n';
+// }
+
+//</> 3
+//=====================
+// function prototypes
+void print_hello();
+void print_world();
+
+// function definitions
+void print_world() {
+    std::cout << "world\n";
+}
+
+void print_hello() {
+    std::cout << "hello, ";
+    print_world();
+}
+
+int main() {
+    print_hello();
+    std::cout << "exit main\n";
 }
