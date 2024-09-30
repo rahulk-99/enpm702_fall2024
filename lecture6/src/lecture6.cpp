06a8058131a5e58736bad1e26429e9a8e9fa92f3
Adon <adon.sf4@gmail.com>
2024-09-29 20:40:33 -0400

    Complete code for lecture6

--- lecture6/src/lecture6.cpp
+++ lecture6/src/lecture6.cpp
@@ -37,6 +37,20 @@ void manage_raw_ptr(int* p) {
     }
 }
 
+void sink_shared_ptr(std::shared_ptr<int> p) {  // Implicit: std::shared_ptr<int> p{s};
+    std::cout << p.use_count() << '\n';         // 2
+}
+
+void reseat_shared_ptr(std::shared_ptr<int>& p) {  // Implicit: std::shared_ptr<int>& p{s};
+    std::cout << p.use_count() << '\n';            // 1
+}
+
+std::shared_ptr<int> return_shared_ptr() {
+    auto p = std::make_shared<int>(10);
+    std::cout << &p << '\n';  // @1
+    return p;
+}
+
 int main() {
     //</> 1
     //=====================
@@ -161,4 +175,119 @@ int main() {
     // auto u{return_by_value()};
     // std::cout << *u << '\n';  // 10
     // std::cout << &u << '\n';  // @1
+
+    //</> 16
+    //=====================
+    // auto s1 = std::make_shared<int>(10);
+    // auto s2{s1};
+    // auto s3 = s2;
+
+    //</> 17
+    //=====================
+    // auto s1 = std::make_shared<int>(10);
+    // std::cout << s1.use_count() << '\n';
+
+    // auto s2{s1};
+    // std::cout << s1.use_count() << ' ';
+    // std::cout << s2.use_count() << '\n';
+
+    // auto s3 = s2;
+    // std::cout << s1.use_count() << ' ';
+    // std::cout << s2.use_count() << ' ';
+    // std::cout << s3.use_count() << '\n';
+
+    // s1.reset();
+    // std::cout << s1.use_count() << ' ';
+    // std::cout << s2.use_count() << ' ';
+    // std::cout << s3.use_count() << '\n';
+
+    // s1.swap(s2);
+    // std::cout << s1.use_count() << ' ';
+    // std::cout << s2.use_count() << ' ';
+    // std::cout << s3.use_count() << '\n';
+
+    // s2.reset(new int(20));
+    // std::cout << s1.use_count() << ' ';
+    // std::cout << s2.use_count() << ' ';
+    // std::cout << s3.use_count() << '\n';
+
+    //</> 18
+    //=====================
+    // auto s = std::make_shared<int>(10);
+    // std::cout << s.use_count() << '\n';  // 1
+    // sink_shared_ptr(s);
+    // std::cout << s.use_count() << '\n';  // 1
+
+    //</> 19
+    //=====================
+    // auto s = std::make_shared<int>(10);
+    // std::cout << s.use_count() << '\n';  // 1
+    // reseat_shared_ptr(s);
+    // std::cout << s.use_count() << '\n';  // 1
+
+    //</> 20
+    //=====================
+    // auto s{return_shared_ptr()};
+    // std::cout << &s << '\n';  // @1
+
+    //</> 21
+    //=====================
+    // auto s = std::make_shared<int>(10);
+    // // Create a weak_ptr from a shared_ptr
+    // std::weak_ptr<int> w1{s};
+    // // Create a weak_ptr from another weak_ptr
+    // std::weak_ptr<int> w2{w1};
+
+    //</> 22
+    //=====================
+    // auto s_ptr = std::make_shared<int>(10);
+    // // create a weak_ptr from a shared_ptr
+    // std::weak_ptr<int> w_ptr{s_ptr};
+
+    // std::cout << *w_ptr << '\n';       // error
+    // std::cout << w_ptr.get() << '\n';  // error
+
+    //</> 23
+    //=====================
+    // // Declare an empty weak pointer
+    // std::weak_ptr<int> w;
+    // {
+    //     auto s1 = std::make_shared<int>(10);
+    //     w = s1;  // w now points to the resource
+    //     if (auto s2 = w.lock(); s2)
+    //         std::cout << "resource is " << *s2 << "\n";
+    //     else
+    //         std::cout << "unable to get resource\n";
+    // }  // s1 goes out of scope and the resource is deleted
+
+    // if (auto s2 = w.lock(); s2)
+    //     std::cout << "resource is =" << *s2 << "\n";
+    // else
+    //     std::cout << "unable to get resource\n";
+
+    //</> 24
+    //=====================
+    // // Declare an empty weak pointer
+    // std::weak_ptr<int> w;
+    // {
+    //     std::shared_ptr<int> t;
+    //     auto s1 = std::make_shared<int>(10);
+    //     w = s1;  // w now points to the resource
+    //     if (!w.expired())
+    //         std::cout << "resource is valid\n";
+    //     else
+    //         std::cout << "resource is expired\n";
+    // }  // s1 goes out of scope and the resource is deleted
+
+    // if (!w.expired())
+    //     std::cout << "resource is valid\n";
+    // else
+    //     std::cout << "resource is expired\n";
+
+    //</> 25
+    //=====================
+    // auto s = std::make_shared<int>(10);
+    // std::weak_ptr<int> w = s;
+    // s.reset();
+    // w.reset();
 }
